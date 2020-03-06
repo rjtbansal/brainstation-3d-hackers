417e5cd97436b779fd5fac237a14ba199fbaf9b1
Jacob Bayisa <bayisagobu@gmail.com>
2020-03-06 14:44:31 -0500

    header and pages background styled

---
+++ client/src/components/Developers/Developers.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Developers.scss';
+
+const Developers = () =>{
+    return (
+        <div className="developers">
+            <h1>Developers</h1>
+        </div>
+    )
+}
+
+export default Developers;
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,10 +5,9 @@ const Header = ()=>{
     return(
         <div className="header">
             <ul className="header__nav-link">
-                <Link to ="/" className="header__router-Link"> <li> PlaceHolder </li> </Link>
-                <Link to ="/sidebar" className="header__router-Link"> <li> PlaceHolder </li></Link>
-                <li> PlaceHolder </li>
-                <li> PlaceHolder </li>
+                <Link to ="/" className="header__router-Link"> <li> Welcome </li> </Link>
+                <Link to ="/socialMedia" className="header__router-Link"> <li> Social Media Managers </li></Link>
+                <Link to ="/developers" className ="header__router-Link"> <li> Developers </li> </Link>
             </ul>
         </div>
     )
