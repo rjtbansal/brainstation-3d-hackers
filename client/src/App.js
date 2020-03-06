1ce98d9e837b148349dc0c1c2e691d60b0aa3cda
Rajat Bansal <rjtbansal@gmail.com>
2020-03-06 10:49:16 -0500

    modified app.js and wired them up

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.scss';
 
-import { Helmet } from 'react-helmet';
+import  Helmet  from 'react-helmet';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 
 function App() {
   return (
@@ -13,14 +15,17 @@ function App() {
       <Helmet>
         <title>Your App Title Here</title>
       </Helmet>
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <h2>Brainstation 3D Starter</h2>
         <h3> Team number 10: The Hackers</h3>
-      </header>
+
+      </header> */}
+      <Header />
+      <Main />
     </div>
   );
 }
