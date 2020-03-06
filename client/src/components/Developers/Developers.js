c9a8ad5208af2e0d39ef7e2a580343548cde1c3d
Rajat Bansal <rjtbansal@gmail.com>
2020-03-06 16:10:39 -0500

    graphs added

--- client/src/components/Developers/Developers.js
+++ client/src/components/Developers/Developers.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Developers.scss';
-
+import developersImage from '../../assets/images/PNG/developers-dashboard.png'
 const Developers = () =>{
     return (
         <div className="developers">
-            <iframe className="developers__iframe" src="https://public.tableau.com/shared/9SYCPN5YB?:display_count=n&:origin=viz_share_link?:showVizHome=n:embed=y&:display_count=yes&publish=yes&:origin=viz_share_link"></iframe>
+            {/* <iframe className="developers__iframe" src="https://public.tableau.com/shared/9SYCPN5YB?:display_count=n&:origin=viz_share_link?:showVizHome=n:embed=y&:display_count=yes&publish=yes&:origin=viz_share_link"></iframe> */}
+            <img className="developers__image" src={developersImage} alt="developers-image" />
         </div>
     )
 }
--- client/src/components/Social/Social.js
+++ client/src/components/Social/Social.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Social.scss';
-
+import socialMediaDashboard from '../../assets/images/PNG/social-media-dashboard.png';
 const Social = () =>{
     return (
-        <div className="social">
-                    <iframe className="main-page__iframe" src="https://public.tableau.com/shared/JHZW4HP3S?:display_count=n&:origin=viz_share_link?:display_count=n&:origin=viz_share_link?:showVizHome=n:embed=y&:display_count=yes&publish=yes&:origin=viz_share_link"></iframe>
+        <div>
+                    {/* <iframe className="main-page__iframe" src="https://public.tableau.com/shared/JHZW4HP3S?:display_count=n&:origin=viz_share_link?:display_count=n&:origin=viz_share_link?:showVizHome=n:embed=y&:display_count=yes&publish=yes&:origin=viz_share_link"></iframe> */}
+             <img className="social-media-image" src={socialMediaDashboard} alt="social media dashboard" />
         </div>
     )
 }
