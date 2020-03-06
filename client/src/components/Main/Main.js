760d65d5279d1ed851a5d08f1e1d1ff7683baeb0
Rajat Bansal <rjtbansal@gmail.com>
2020-03-06 14:17:47 -0500

    rendering a tableau iframe

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -4,7 +4,10 @@ import './Main.scss';
 
 const Main = () => {
     return(
-        <h1>This is Main page </h1>
+        // <h1>This is Main page </h1>
+        <div className="main-page">
+            <iframe className="main-page__iframe" src="https://public.tableau.com/shared/9SYCPN5YB?:display_count=n&:origin=viz_share_link?:showVizHome=n:embed=y&:display_count=yes&publish=yes&:origin=viz_share_link"></iframe>
+        </div>
     )
 }
 
