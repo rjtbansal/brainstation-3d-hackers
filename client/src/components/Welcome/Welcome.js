354798755964f24765bb76e06bb4a46aea804e3b
Rajat Bansal <rjtbansal@gmail.com>
2020-03-06 15:55:16 -0500

    styled welcome page

--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -12,21 +12,23 @@ const Main = () => {
             <div className="welcome__title-div">
                 <h2 className="welcome__title">BUSINESS TECH TRENDS</h2>
             </div>
-            <div>
-                <img src={arGlasses} alt="ar glasses image" />
-                <p className="welcome__text">Were you always wondering, 
-                    “jeeze, I wish I knew which province had the greatest job opportunities for social media managers”?
-                </p>
-            </div>
-            <div>
-                <h3 className="welcome__text2">Welcome</h3>
-            </div>
-            <div>
-                <img src = {cube} alt="cube-icon" />
-                <p className="welcome__text">
-                Well, you came to the right place. This website allows you to see digital tech trends in various industries across all of Canada!
-                </p>
-            </div>
+            <section className="welcome__section">
+                <div>
+                    <img src={arGlasses} alt="ar glasses image" />
+                    <p className="welcome__text">Were you always wondering, 
+                        “jeeze, I wish I knew which province had the greatest job opportunities for social media managers”?
+                    </p>
+                </div>
+                <div>
+                    <h3 className="welcome__text2">Welcome</h3>
+                </div>
+                <div>
+                    <img src = {cube} alt="cube-icon" />
+                    <p className="welcome__text">
+                    Well, you came to the right place. This website allows you to see digital tech trends in various industries across all of Canada!
+                    </p>
+                </div>
+            </section>
         </div>
     )
 }
