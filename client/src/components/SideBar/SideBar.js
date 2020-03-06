d8a1be941c4e845ecd320f21b6ecb4ee1ea99e20
Rajat Bansal <rjtbansal@gmail.com>
2020-03-06 14:49:55 -0500

    rendering 2nd tableu iframe

--- client/src/components/SideBar/SideBar.js
+++ client/src/components/SideBar/SideBar.js
@@ -3,7 +3,8 @@ import './SideBar.scss';
 
 const SideBar = () =>{
     return(
-        <h1> This is side bar</h1>
+        <iframe className="main-page__iframe" src="https://public.tableau.com/shared/JHZW4HP3S?:display_count=n&:origin=viz_share_link?:display_count=n&:origin=viz_share_link?:showVizHome=n:embed=y&:display_count=yes&publish=yes&:origin=viz_share_link"></iframe>
+
     )
 }
 
